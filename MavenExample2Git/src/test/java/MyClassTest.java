f827584d93747665f98ed3202d35375ff84ce42b
Sudhir Mokashi <90191498+sudhirmokashi@users.noreply.github.com>
2022-07-15 15:25:59 +0530

    Update MyClassTest.java

--- MavenExample2Git/src/test/java/MyClassTest.java
+++ MavenExample2Git/src/test/java/MyClassTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.lti.entity.MyClass;
-import com.sun.xml.internal.ws.policy.AssertionSet;
 
 class MyClassTest {
 	
